65409f84629168b2ee95da7e6c166a6d7092bb2c
Ross Morgan <ross.morgan933@gmail.com>
2024-09-10 20:56:53 -0500

    post DTOs added and setup complete

---
+++ DTOs/PostTagDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace RareAPI_BE.DTOs
+{
+    public class PostTagDTO
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public int TagId  { get; set; }
+
+    }
+}
