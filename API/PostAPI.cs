eed1c270a7e639e703d7bfe00afe08b2af89bc16
Ross Morgan <ross.morgan933@gmail.com>
2024-09-11 18:39:03 -0500

    view by post id completed

--- API/PostAPI.cs
+++ API/PostAPI.cs
@@ -8,10 +8,27 @@ namespace RareAPI_BE.API
     {
         public static void Map(WebApplication app)
         {
-            // Get All Artwork + tags
+            // Get All Posts + tags
             app.MapGet("/post", (RareAPI_BEDbContext db) =>
             {
                 return db.Posts.Include(a => a.PostTags).ThenInclude(t => t.Tag).ToList();
+            });
+
+            // Get Single Post
+            app.MapGet("/post/{id}", (RareAPI_BEDbContext db, int id) =>
+            {
+                var postId = db.Posts
+                .Include(a => a.PostTags)
+                .ThenInclude(t => t.Tag)
+                .FirstOrDefault(a => a.Id == id);
+
+
+                if (postId == null)
+                {
+                    return Results.NotFound("No Artwork Found.");
+                }
+
+                return Results.Ok(postId);
             });
 
         }
