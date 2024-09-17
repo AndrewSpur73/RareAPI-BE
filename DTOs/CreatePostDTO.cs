79263a818b181d35b99270bb3d59c3b063507ee0
Andrew <andrewspurlock@rocketmail.com>
2024-09-17 18:27:33 -0500

    Create post DTO updated

--- DTOs/CreatePostDTO.cs
+++ DTOs/CreatePostDTO.cs
@@ -2,10 +2,10 @@
 {
     public class CreatePostDTO
     {
+        public int UserId { get; set; }
         public string? Title { get; set; }
         public string? Content { get; set; }
         public string? ImageUrl { get; set; }
-
         public List<int>? TagIds { get; set; } = new List<int>();
     }
 }
